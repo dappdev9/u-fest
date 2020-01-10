1a626c96e91cab2de52cef7c2f4a17f9ead5c730
dappdev9 <dappdev9@gmail.com>
2020-01-10 16:07:57 +0300

    up

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -18,8 +18,9 @@
 </style>
 
 <div class="bg-teal-700">
+    <span class="text-gray-300">Ufest</span>
     <button type="button" class="text-white">
-        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
         </svg>
     </button>
