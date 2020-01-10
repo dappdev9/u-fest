fa2a69d42222f27f6c255b7638514f4bd325cb2a
dappdev9 <dappdev9@gmail.com>
2020-01-10 11:47:49 +0300

    up

--- src/components/CardsMain.svelte
+++ src/components/CardsMain.svelte
@@ -8,12 +8,12 @@
 
 <style>
     img {
-        height: 100px;
+        height: 150px;
     }
 </style>
 
 <div class="bg-white border rounded-lg overflow-hidden">
-    <img class="object-cover" src={foto} alt={title}>
+    <img class="object-cover w-full" src={foto} alt={title}>
     <div class="p-6 ">
         <h3>{title}</h3>
         <div>
