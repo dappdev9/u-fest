dc477f2b356f8632159d4dba1c5012484814f5a9
dappdev9 <dappdev9@gmail.com>
2020-01-10 13:08:09 +0300

    up

--- src/components/CardsMain.svelte
+++ src/components/CardsMain.svelte
@@ -20,7 +20,7 @@
         <div class="mt-4 text-sm text-gray-700">
             {desc}
         </div>
-        <div class="mt-6 text-teal-500 text-sm">
+        <div class="mt-6 text-teal-500 font-medium text-sm text-justify">
             <span>
                 <svg class="h-4 w-4 fill-current text-teal-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
