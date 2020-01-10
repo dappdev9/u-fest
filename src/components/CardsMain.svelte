575786e276023647e80f9c144861de6c8373f305
dappdev9 <dappdev9@gmail.com>
2020-01-10 11:44:47 +0300

    up

--- src/components/CardsMain.svelte
+++ src/components/CardsMain.svelte
@@ -14,7 +14,7 @@
 </style>
 
 <div class="bg-white border rounded-lg overflow-hidden">
-    <img src={foto} alt={title}>
+    <img class="object-cover" src={foto} alt={title}>
     <div class="p-6 ">
         <h3>{title}</h3>
         <div>
