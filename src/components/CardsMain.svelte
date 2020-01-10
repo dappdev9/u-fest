ea75ed0221b2197c934374d49b97457fc271f6a0
dappdev9 <dappdev9@gmail.com>
2020-01-10 12:14:51 +0300

    up

--- src/components/CardsMain.svelte
+++ src/components/CardsMain.svelte
@@ -13,7 +13,7 @@
 </style>
 
 <div class="bg-white border rounded-lg overflow-hidden w-64">
-    <img class="object-cover" src={foto} alt={title}>
+    <img class="object-none" src={foto} alt={title}>
     <div class="p-6 ">
         <h3 class="font-semibold text-lg leading-tight truncate">{title}</h3>
         <div class="mt-6">
