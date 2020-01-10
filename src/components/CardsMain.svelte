b9e2762d91d1e53818decb32a60fce39e367761e
dappdev9 <dappdev9@gmail.com>
2020-01-10 13:11:49 +0300

    up

--- src/components/CardsMain.svelte
+++ src/components/CardsMain.svelte
@@ -20,15 +20,15 @@
         <div class="mt-4 text-sm text-gray-700">
             {desc}
         </div>
-        <div class="mt-6 text-teal-500 font-medium text-sm text-justify">
+        <div class="mt-6 text-teal-500 font-medium text-sm tracking-wide">
             <span>
-                <svg class="h-4 w-4 fill-current text-teal-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg class="h-5 w-5 fill-current text-teal-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
                 </svg>
                 {views}
             </span>
             <span>
-                <svg class="h-4 w-4 fill-current text-teal-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg class="h-3 w-3 fill-current text-teal-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/>
                 </svg>
                 {participants}
